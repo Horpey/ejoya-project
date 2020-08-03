e75cba8f8870e50311f9f9b999cc43f2efb52f66
Horpey <adeniran1059@gmail.com>
2020-08-03 19:38:01 +0100

    Calc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,7 +6,17 @@ var app = new Vue({
         minAmt: '',
         maxAmt: '',
         apply: false,
-        alertMsg: ''
+        sending: false,
+        alertMsg: '',
+        formDetails: {
+            firstName: '',
+            lastName: '',
+            artistName: '',
+            spotifyProfile: '',
+            email: '',
+            phoneNumber: '',
+            amount: ''
+        }
     },
     methods: {
         calFund() {
@@ -26,6 +36,34 @@ var app = new Vue({
                 this.alertMsg = 'Enter a valid amount!'
                 this.apply = false
             }
+        },
+        submitDetails() {
+            let userdata = this.formDetails
+            console.log(userdata)
+            this.sending = true;
+            axios.post('https://v2-api.sheety.co/ba50ebf04d8036a163116772ea2c8711/horpeyWallpaper/users', userdata)
+                .then(function (response) {
+                    console.log(this.sending + 'Hello')
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    this.sending = false
+                    console.log(this.sending + ' Hello')
+
+                    // console.log(error);
+                })
+
+            // $.ajax({
+            //     url: 'https://v2-api.sheety.co/ba50ebf04d8036a163116772ea2c8711/horpeyWallpaper/users',
+            //     type: 'POST',
+            //     data: userdata,
+            //     success: function (data) {
+            //         this.sending = false
+            //     },
+            //     error: function (error) {
+            //         this.sending = false
+            //     }
+            // });
         },
         validateAmt(amt) {
             var rgx = /^[0-9]*\.?[0-9]*$/;
